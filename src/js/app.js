a7e8de24175a6edab1a7129ed518f28ee2dfe7b9
gabo180 <gabrieldejesushernandez@gmail.com>
2022-04-26 00:57:24 +0000

    last commit

--- src/js/app.js
+++ src/js/app.js
@@ -41,9 +41,9 @@ function render(variables = {}) {
           <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition} ${variables.socialMediaColor}">
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -66,10 +66,10 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     socialMediaColor: "Cyan",
     // social media usernames
-    twitter: null,
-    github: "gabo180",
-    linkedin: null,
-    instagram: null,
+    twitter: "",
+    github: "",
+    linkedin: "",
+    instagram: "",
     name: "",
     lastname: "",
     role: "",
